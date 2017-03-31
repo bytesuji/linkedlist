f0c9dcef7f8797019a14035760a893dca0411f3a
Albert Brox III <albert@exypno.tech>
2017-03-31 19:46:47 -0400

    made minor changes

---
+++ lltest.cpp
@@ -0,0 +1,68 @@
+#include "linkedlist.tcc"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "/// BEGIN TESTS ///\n\n";
+	node<int> *a = new node<int>;
+	node<int> *b = new node<int>;
+	node<int> *c = new node<int>;
+
+	a->data = 1;
+	b->data = 2;
+	c->data = 3;
+
+	LinkedList<int> ll(a);
+	ll.append(b);
+	ll.append(c);
+
+	node<int> *d = new node<int>;
+	d->data = 0;
+	ll.push(d);
+
+	for(int i = 0; i < ll.size(); ++i)
+		cout << ll[i]->data << " ";
+	cout << endl;
+
+	cout << ll.pop()->data << endl; // leaks memory
+
+	node<int> *e = new node<int>;
+	e->data = 4;
+	ll.append(e);
+
+	for(int i = 0; i < ll.size(); ++i)
+		cout << ll[i]->data << " ";
+	cout << endl;
+
+	node<int> *camsData = new node<int>;
+	camsData->data = 17;
+	ll.insert(100, camsData);
+
+	for(int i = 0; i < ll.size(); ++i)
+		cout << ll[i]->data << " ";
+	cout << endl;
+
+	cout << "POP\n";
+	cout << ll.pop()->data << endl;
+
+	cout << "begin = " << ll.begin()->data << endl;
+	cout << "end = " << ll.end()->data << endl;
+	cout << ll.size() << endl;
+
+	for(int i = 0; i < ll.size(); ++i)
+		cout << ll[i]->data << " ";
+	cout << endl;
+
+	ll.remove(10);
+
+	for(int i = 0; i < ll.size(); ++i)
+		cout << ll[i]->data << " ";
+	cout << endl;
+	ll.print();
+
+	cout << "\n/// END TESTS ///\n";
+
+	return 0;
+}
